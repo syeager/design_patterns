61859ce903eb5b0e3ee25b9006291933a953d3dc
Steve Yeager <steve.f.yeager@gmail.com>
2024-02-02 16:58:26 -0800

    Add code implementation

---
+++ DesignPatterns/DesignPatterns.Source/Creational/AbstractFactory.cs
@@ -0,0 +1,43 @@
+﻿namespace DesignPatterns.Source.Creational;
+
+#region Interfaces
+
+public interface IWeaponFactory
+{
+    IDefensiveWeapon CreateDefensiveWeapon();
+    IOffensiveWeapon CreateOffensiveWeapon();
+}
+
+public interface IDefensiveWeapon;
+
+public interface IOffensiveWeapon;
+
+#endregion
+
+#region Implementation 1
+
+public sealed class SciFiWeaponFactory : IWeaponFactory
+{
+    public IDefensiveWeapon CreateDefensiveWeapon() => new EnergyShield();
+    public IOffensiveWeapon CreateOffensiveWeapon() => new PlasmaBlaster();
+}
+
+public sealed record EnergyShield : IDefensiveWeapon;
+
+public sealed record PlasmaBlaster : IOffensiveWeapon;
+
+#endregion
+
+#region Implementation 2
+
+public sealed class MedievalFactory : IWeaponFactory
+{
+    public IDefensiveWeapon CreateDefensiveWeapon() => new WoodenShield();
+    public IOffensiveWeapon CreateOffensiveWeapon() => new TwinDaggers();
+}
+
+public sealed record WoodenShield : IDefensiveWeapon;
+
+public sealed record TwinDaggers : IOffensiveWeapon;
+
+#endregion
---
+++ DesignPatterns/DesignPatterns.Test/AbstractFactoryTest.cs
@@ -0,0 +1,32 @@
+﻿using DesignPatterns.Source.Creational;
+
+namespace DesignPatterns.Test;
+
+public sealed class AbstractFactoryTest
+{
+    [Test]
+    public void SwitchFactories()
+    {
+        IWeaponFactory weaponFactory = new SciFiWeaponFactory();
+
+        var defense = weaponFactory.CreateDefensiveWeapon();
+        var offense = weaponFactory.CreateOffensiveWeapon();
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(defense, Is.TypeOf<EnergyShield>());
+            Assert.That(offense, Is.TypeOf<PlasmaBlaster>());
+        });
+
+        weaponFactory = new MedievalFactory();
+
+        defense = weaponFactory.CreateDefensiveWeapon();
+        offense = weaponFactory.CreateOffensiveWeapon();
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(defense, Is.TypeOf<WoodenShield>());
+            Assert.That(offense, Is.TypeOf<TwinDaggers>());
+        });
+    }
+}
