c42d860845c6285d74636592aca382a967edc5ed
Steve Yeager <steve.f.yeager@gmail.com>
2024-02-07 12:14:33 -0800

    Add Command implementation

---
+++ DesignPatterns/DesignPatterns.Source/Behavioral/Command.cs
@@ -0,0 +1,44 @@
+﻿namespace DesignPatterns.Source.Behavioral;
+
+public class Buff(IBuffTarget target, string type)
+{
+    private int buffId;
+
+    public void Apply()
+    {
+        buffId = target.AddBuff(type);
+    }
+
+    public void Remove()
+    {
+        target.RemoveBuff(buffId);
+    }
+}
+
+public interface IBuffTarget
+{
+    int AddBuff(string type);
+    void RemoveBuff(int id);
+}
+
+public class Player : IBuffTarget
+{
+    private int buffIndex;
+    private Dictionary<int, string> buffs = new();
+
+    public int AddBuff(string type)
+    {
+        buffs.Add(++buffIndex, type);
+        return buffIndex;
+    }
+
+    public void RemoveBuff(int id)
+    {
+        buffs.Remove(id);
+    }
+
+    public List<string> ListBuffs()
+    {
+        return buffs.Select(pair => $"{pair.Key}: {pair.Value}").ToList();
+    }
+}
---
+++ DesignPatterns/DesignPatterns.Test/Behavioral/CommandTest.cs
@@ -0,0 +1,33 @@
+﻿using DesignPatterns.Source.Behavioral;
+
+namespace DesignPatterns.Test.Behavioral;
+
+public class CommandTest
+{
+    [Test]
+    public void Run()
+    {
+        var player = new Player();
+        var buffs = new List<Buff>
+        {
+            new(player, "iron-hide"),
+            new(player, "flame-hands"),
+            new(player, "iron-hide"),
+        };
+
+        PrintBuffs(player);
+        foreach (var buff in buffs)
+        {
+            buff.Apply();
+        }
+        PrintBuffs(player);
+        buffs[1].Remove();
+        PrintBuffs(player);
+        buffs[0].Remove();
+        buffs[2].Remove();
+        PrintBuffs(player);
+        return;
+
+        static void PrintBuffs(Player player) => Console.WriteLine($"Buffs:{Environment.NewLine}{string.Join(Environment.NewLine, player.ListBuffs())}");
+    }
+}
