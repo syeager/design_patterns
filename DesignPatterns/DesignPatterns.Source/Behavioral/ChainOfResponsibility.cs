6a7f36d4029ce004e12f59b9dacd8ba0d2e74373
Steve Yeager <steve.f.yeager@gmail.com>
2024-02-07 08:50:00 -0800

    Add Chain of Responsibility implementation

---
+++ DesignPatterns/DesignPatterns.Source/Behavioral/ChainOfResponsibility.cs
@@ -0,0 +1,70 @@
+﻿namespace DesignPatterns.Source.Behavioral;
+
+public class Score
+{
+    public int Value { get; set; }
+}
+
+public interface IScoreHandler
+{
+    void Handle(Score score);
+}
+
+public abstract class ScoreHandler(IScoreHandler next) : IScoreHandler
+{
+    public virtual void Handle(Score score)
+    {
+        next.Handle(score);
+    }
+}
+
+public class Game(IScoreHandler root)
+{
+    public int Play()
+    {
+        var score = new Score();
+        root.Handle(score);
+        return score.Value;
+    }
+}
+
+public class Double(IScoreHandler next) : ScoreHandler(next)
+{
+    public override void Handle(Score score)
+    {
+        score.Value *= 2;
+        base.Handle(score);
+    }
+}
+
+public class PlusOne(IScoreHandler next) : ScoreHandler(next)
+{
+    public override void Handle(Score score)
+    {
+        score.Value++;
+        base.Handle(score);
+    }
+}
+
+public class EndGameIfOdd(IScoreHandler next) : ScoreHandler(next)
+{
+    public override void Handle(Score score)
+    {
+        if (score.Value % 2 == 0)
+        {
+            base.Handle(score);
+        }
+        else
+        {
+            Console.WriteLine("Ending because of odd number!");
+        }
+    }
+}
+
+public class EndGame() : ScoreHandler(null)
+{
+    public override void Handle(Score score)
+    {
+        Console.WriteLine("OVER!");
+    }
+}
---
+++ DesignPatterns/DesignPatterns.Test/Behavioral/ChainOfResponsibilityTest.cs
@@ -0,0 +1,24 @@
+﻿using DesignPatterns.Source.Behavioral;
+using Double = DesignPatterns.Source.Behavioral.Double;
+
+namespace DesignPatterns.Test.Behavioral;
+
+public class ChainOfResponsibilityTest
+{
+    [Test]
+    public void Run()
+    {
+        var root =
+            new PlusOne(
+                new PlusOne(
+                    new Double(
+                        new EndGameIfOdd(
+                            new PlusOne(
+                                new EndGameIfOdd(
+                                    new EndGame()))))));
+
+        var game = new Game(root);
+
+        Console.WriteLine(game.Play());
+    }
+}
