f445e63f98b32883f498f7686a7f21256c4aabc1
Steve Yeager <steve.f.yeager@gmail.com>
2024-02-08 09:03:38 -0800

    Add Iterator implementation

---
+++ DesignPatterns/DesignPatterns.Source/Behavioral/Iterator.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace DesignPatterns.Source.Behavioral;
+
+public interface IIterator
+{
+    bool MoveNext([NotNullWhen(true)] out string? entry);
+}
+
+public class RandomIterator(IList<string> list) : IIterator
+{
+    private readonly IList<int> remainingIndices = list.Select((_, i) => i).ToList();
+
+    public bool MoveNext([NotNullWhen(true)] out string? entry)
+    {
+        if (remainingIndices.Count == 0)
+        {
+            entry = null;
+            return false;
+        }
+
+        var index = Random.Shared.Next(0, remainingIndices.Count);
+        entry = list[remainingIndices[index]];
+        remainingIndices.RemoveAt(index);
+        return remainingIndices.Count > 0;
+    }
+}
+
+public class BackwardsIterator(IList<string> list) : IIterator
+{
+    private int index = list.Count - 1;
+
+    public bool MoveNext([NotNullWhen(true)] out string? entry)
+    {
+        if (index < 0)
+        {
+            entry = null;
+            return false;
+        }
+
+        entry = list[index--];
+        return index >= 0;
+    }
+}
---
+++ DesignPatterns/DesignPatterns.Test/Behavioral/IteratorTest.cs
@@ -0,0 +1,38 @@
+﻿using DesignPatterns.Source.Behavioral;
+
+namespace DesignPatterns.Test.Behavioral;
+
+public class IteratorTest
+{
+    [Test]
+    public void Run()
+    {
+        var items = new List<string>
+        {
+            "apple",
+            "bat",
+            "carrot",
+            "dog"
+        };
+
+        var random = new RandomIterator(items);
+        for (var i = 0; i < items.Count; i++)
+        {
+            PrintNext(random);
+        }
+
+        var backwards = new BackwardsIterator(items);
+        for (var i = 0; i < items.Count; i++)
+        {
+            PrintNext(backwards);
+        }
+
+        return;
+
+        static void PrintNext(IIterator iterator)
+        {
+            var hasMore = iterator.MoveNext(out var entry);
+            Console.WriteLine($"{iterator.GetType().Name}: {entry}{(hasMore ? "" : " Done")}");
+        }
+    }
+}
